47bbe95d7a9fde64735b4f775cc60a2e9ea7e781
zhenlu <zhenlu@1936@gmail.com>
2024-11-29 12:57:03 +0800

    新增了测试用例.

--- tests/executorTests.cpp
+++ tests/executorTests.cpp
@@ -88,3 +88,113 @@ TEST(ExecutorTest, x1_y1_E_use_FR_to_x2_y1_S) {
 	pExecutor->Execute(command);
 	ASSERT_EQ(pExecutor->Query(), target);
 }
+
+TEST(ExecutorTest, x0_y0_E_use_BM_to_xn1_y0_E) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, true, -1, 0, 'E');
+	std::string command = "BM";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_BL_to_x0_y0_S) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, true, 0, 0, 'S');
+	std::string command = "BL";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_BR_to_x0_y0_N) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, true, -1, 0, 'N');
+	std::string command = "BR";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_FM_to_x2_y0_E) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, false, 2, 0, 'E');
+	std::string command = "FM";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_FL_to_x1_y0_N) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, false, 1, 0, 'N');
+	std::string command = "FL";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_FR_to_x1_y0_S) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(false, false, 1, 0, 'S');
+	std::string command = "BR";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_BFM_to_xn2_y0_E) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(true, true, -2, 0, 'E');
+	std::string command = "BFM";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_BFL_to_xn1_y0_S) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(true, true, -1, 0, 'S');
+	std::string command = "BFL";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_E_use_BFR_to_xn1_y0_N) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'E'}));
+	PoseHandler target(true, true, -1, 0, 'N');
+	std::string command = "BFR";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_N_use_BBM_to_x0_y1_N) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'N'}));
+	PoseHandler target(false, false, 0, 1, 'N');
+	std::string command = "BFM";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
+
+TEST(ExecutorTest, x0_y0_N_use_FFM_to_x0_y1_N) {
+	std::unique_ptr<ExecutorImpl> pExecutor(
+		ExecutorImpl::NewExecutor({false, false, 0, 0, 'N'}));
+	PoseHandler target(false, false, 0, 1, 'N');
+	std::string command = "BFM";
+
+	pExecutor->Execute(command);
+	ASSERT_EQ(pExecutor->Query(), target);
+}
